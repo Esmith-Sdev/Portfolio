ad642240697950db8a13d68cf7f92fa7ef75484b
Esmith-Sdev <esmith897@ivytech.edu>
2025-10-22 20:12:27 -0400

    Navbar Feature/Responsive styles

--- portfolio/src/Components/NavigationBar.jsx
+++ portfolio/src/Components/NavigationBar.jsx
@@ -1,15 +1,44 @@
 import { Navbar, Nav, Container, Image } from "react-bootstrap";
+import { useState } from "react";
+import { Divide as Hamburger } from "hamburger-react";
 import "../styles/Navbar.css";
-import Logo from "../assets/ES-LOGO.png";
+import Logo from "../assets/ES-PURPLE.png";
 export default function NavigationBar() {
+  const [isOpen, setOpen] = useState(false);
   return (
-    <Navbar className="navigationBar" expand="lg" fixed="top">
+    <Navbar
+      className="navigationBar"
+      expand="lg"
+      fixed="top"
+      expanded={isOpen}
+      onToggle={setOpen}
+      collapseOnSelect={isOpen}
+    >
       <Container fluid>
         <Navbar.Brand className="navbrand" href="/Portfolio/#welcome">
-          <Image className="navBrandLogo" src={Logo}></Image>
+          <Image className="navBrandLogo" src={Logo} alt="Logo" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll"></Navbar.Toggle>
-        <Navbar.Collapse className="justify-content-center" id="navbarScroll">
+
+        <Navbar.Toggle
+          as="button"
+          className="navbar-toggler border-0 p-0 d-lg-none"
+          aria-controls="main-nav"
+          aria-expanded={isOpen}
+          aria-label="Toggle navigation"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          <Hamburger
+            color="#a882ff"
+            toggled={isOpen}
+            toggle={setOpen}
+            size={30}
+          />
+        </Navbar.Toggle>
+
+        <Navbar.Collapse
+          id="main-nav"
+          className="d-lg-flex justify-content-center"
+        >
           <Nav variant="underline" defaultActiveKey="#welcome">
             <Nav.Item>
               <Nav.Link className="navLink" href="/Portfolio/#welcome">
--- portfolio/src/Pages/Projects.jsx
+++ portfolio/src/Pages/Projects.jsx
@@ -21,103 +21,150 @@ export default function Projects() {
         >
           <span className="gradientSpan">Projects</span>
         </h1>
-        <Container className="d-flex flex-row justify-content-center align-items-center gap-5">
-          <Card
-            className="d-flex justify-content-center align-items-center flex-column"
-            style={{
-              width: "30rem",
-              marginTop: "5rem",
-              border: "2px solid #a882ff",
-              background: "transparent",
-            }}
-          >
-            <Card.Img
+        <Container className="d-flex flex-column justify-content-center align-items-center gap-5">
+          <Container className="d-flex flex-column flex-lg-row justify-content-center align-items-center gap-5">
+            <Card
+              className="d-flex justify-content-center align-items-center flex-column"
               style={{
-                width: "200px",
-                height: "200px",
-                overflow: "hidden",
+                width: "30rem",
+                marginTop: "5rem",
+                border: "2px solid #a882ff",
+                background: "transparent",
               }}
-              variant="top"
-              src={GuesswhoThumbnail}
-            ></Card.Img>
-            <Card.Body className="d-flex justify-content-center align-items-center flex-column">
-              <h2>
-                <span className="h2Span">Guess Who</span>
-              </h2>
-              <p className="cardText">
-                This is a website I made based off the game Guess Who. I added
-                my own twist to the game by allowing users to use their own
-                images or choose from templates.
-              </p>
-              <button style={{ width: "10rem" }} className="flashyButton">
-                View
-              </button>
-            </Card.Body>
-          </Card>
-          <Card
-            className="d-flex justify-content-center align-items-center flex-column"
-            style={{
-              width: "30rem",
-              marginTop: "5rem",
-              border: "2px solid #a882ff",
-              background: "transparent",
-            }}
-          >
-            <Card.Img
+            >
+              <Card.Img
+                style={{
+                  width: "200px",
+                  height: "200px",
+                  overflow: "hidden",
+                }}
+                variant="top"
+                src={GuesswhoThumbnail}
+              ></Card.Img>
+              <Card.Body className="d-flex justify-content-center align-items-center flex-column">
+                <h2>
+                  <span className="h2Span">Guess Who</span>
+                </h2>
+                <p className="cardText">
+                  This is a website I made based off the game Guess Who. I added
+                  my own twist to the game by allowing users to use their own
+                  images or choose from templates.
+                </p>
+                <a
+                  target="_blank"
+                  href="https://guesswho-kfk5.onrender.com"
+                  style={{ width: "10rem" }}
+                  className="flashyButton"
+                >
+                  View
+                </a>
+              </Card.Body>
+            </Card>
+            <Card
+              className="d-flex justify-content-center align-items-center flex-column"
               style={{
-                width: "200px",
-                height: "200px",
-                overflow: "hidden",
+                width: "30rem",
+                marginTop: "5rem",
+                border: "2px solid #a882ff",
+                background: "transparent",
               }}
-              variant="top"
-              src={FishQuestThumbnail}
-            ></Card.Img>
-            <Card.Body className="d-flex justify-content-center align-items-center flex-column">
-              <h2>
-                <span className="h2Span">FishQuest</span>
-              </h2>
-              <p className="cardText">
-                Currently this app is still in early stages of development. I am
-                taking on the project alone, but I have made progress with the
-                mockup and prototype.
-              </p>
-              <button style={{ width: "10rem" }} className="flashyButton">
-                View
-              </button>
-            </Card.Body>
-          </Card>
-          <Card
-            className="d-flex justify-content-center align-items-center flex-column"
-            style={{
-              width: "30rem",
-              marginTop: "5rem",
-              border: "2px solid #a882ff",
-              background: "transparent",
-            }}
-          >
-            <Card.Img
+            >
+              <Card.Img
+                style={{
+                  width: "200px",
+                  height: "200px",
+                  overflow: "hidden",
+                }}
+                variant="top"
+                src={FishQuestThumbnail}
+              ></Card.Img>
+              <Card.Body className="d-flex justify-content-center align-items-center flex-column">
+                <h2>
+                  <span className="h2Span">FishQuest</span>
+                </h2>
+                <p className="cardText">
+                  Currently this app is still in early stages of development. I
+                  am taking on the project alone, but I have made progress with
+                  the mockup and prototype.
+                </p>
+                <a
+                  target="_blank"
+                  href="https://www.figma.com/proto/Q1O2KU3hjixDBYz1PuyAda/FishQuest-Wireframe.mockup_--Copy-?t=PP10RiQX7fmWVD8Q-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&node-id=1-2&starting-point-node-id=1%3A2"
+                  style={{ width: "10rem" }}
+                  className="flashyButton"
+                >
+                  View
+                </a>
+              </Card.Body>
+            </Card>
+            <Card
+              className="d-flex justify-content-center align-items-center flex-column"
               style={{
-                width: "200px",
-                height: "200px",
-                overflow: "hidden",
+                width: "30rem",
+                marginTop: "5rem",
+                border: "2px solid #a882ff",
+                background: "transparent",
               }}
-              variant="top"
-              src={SheinThumbnail}
-            ></Card.Img>
-            <Card.Body className="d-flex justify-content-center align-items-center flex-column">
-              <h2>
-                <span className="h2Span">Shein Rebrand</span>
-              </h2>
-              <p className="cardText">
-                The SHEIN redesign was a UX/UI project focused on improving
-                trust and simplicity by modernizing the layout and creating a
-                more enjoyable shopping experience.
-              </p>
-              <button style={{ width: "10rem" }} className="flashyButton">
-                View
-              </button>
-            </Card.Body>
-          </Card>
+            >
+              <Card.Img
+                style={{
+                  width: "200px",
+                  height: "200px",
+                  overflow: "hidden",
+                }}
+                variant="top"
+                src={SheinThumbnail}
+              ></Card.Img>
+              <Card.Body className="d-flex justify-content-center align-items-center flex-column">
+                <h2>
+                  <span className="h2Span">Shein Rebrand</span>
+                </h2>
+                <p className="cardText">
+                  The SHEIN redesign was a UX/UI project focused on improving
+                  trust and simplicity by modernizing the layout and creating a
+                  more enjoyable shopping experience.
+                </p>
+                <a
+                  href="https://www.figma.com/proto/XCqkp0fJeMkDoG9ruQDHDU/Mockup?node-id=3688-6098&p=f&t=Amy4hbRpOO9OiY9x-1&scaling=min-zoom&content-scaling=fixed&page-id=3688%3A5982&starting-point-node-id=3688%3A6098"
+                  target="_blank"
+                  style={{ width: "10rem" }}
+                  className="flashyButton"
+                >
+                  View
+                </a>
+              </Card.Body>
+            </Card>
+          </Container>
+          <Container className="d-flex flex-row justify-content-center align-items-center gap-5">
+            <Card
+              className="d-flex justify-content-center align-items-center flex-column"
+              style={{
+                width: "30rem",
+                marginTop: "5rem",
+                border: "2px solid #a882ff",
+                background: "transparent",
+              }}
+            >
+              <Card.Body className="d-flex justify-content-center align-items-center flex-column">
+                <h2 className="p-3">
+                  <span className="h2Span">CourseBuilder.io</span>
+                </h2>
+                <p className="cardText">
+                  This is a React-based school project designed to practice API
+                  integration and provide students and teachers with tools to
+                  manage and organize their courses efficiently.
+                </p>
+                <a
+                  target="_blank"
+                  href="https://esmith-sdev.github.io/SDEV_255_Final_Project_Group_8"
+                  style={{ width: "10rem" }}
+                  className="flashyButton"
+                >
+                  View
+                </a>
+              </Card.Body>
+            </Card>
+          </Container>
         </Container>
       </div>
     </Container>
